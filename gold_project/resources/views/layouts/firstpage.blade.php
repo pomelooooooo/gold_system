647298734e26fa9fe4af70f9d902c0d4817e86b0
pomelooooooo <suwijak1691@gmail.com>
2021-11-18 23:20:22 +0700

    edit css font first page

--- gold_project/resources/views/layouts/firstpage.blade.php
+++ gold_project/resources/views/layouts/firstpage.blade.php
@@ -21,6 +21,11 @@
     <!-- google font -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Chonburi&display=swap" rel="stylesheet">
+    
     <!-- fontawesome -->
     <link rel="stylesheet" href="{{url('assets/css/all.min.css')}}">
     <!-- bootstrap -->
